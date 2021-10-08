dd3eafe6e8462f285ee087b49c1c3ba847bcd9e1
5oN1A <guarino.sonia@gmail.com>
2021-10-08 16:22:01 +0200

    Completed

--- js/script.js
+++ js/script.js
@@ -5,5 +5,37 @@
 4.calcolo prezzo del biglietto in base ai km 
 5.applicazione degli eventuali sconti in riferimento all'età passeggero
 6.arrotondo i valori del prezzo finale
-
 */
+
+let tripLenght = parseInt(prompt("Inserisci il numero di Kilometri da percorrere"));
+
+if (isNaN(tripLenght)) {
+    alert("il valore inserito non è valido");
+}
+
+let userAge = parseInt(prompt("inserisci la tua età"));
+
+let tripPrice = tripLenght * 0.21;
+
+let prezzoScontato;
+
+if (isNaN(userAge)) {
+    alert("il valore inserito non è valido");
+} else {
+    if (userAge < 18) {
+        prezzoScontato = tripPrice - (tripPrice * 0.2);
+        alert("il prezzo del tuo biglietto è " + prezzoScontato.toFixed(2));
+    } else if (userAge >= 65) {
+        prezzoScontato = tripPrice - (tripPrice * 0.4);
+        alert("il prezzo del tuo biglietto scontato è di:" + prezzoScontato.toFixed(2));
+
+    } else {
+        alert("il prezzo del tuo biglietto scontato è di:" + tripPrice.toFixed(2));
+    }
+}
+
+
+
+
+
+
