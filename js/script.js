341475a8d8af70405b054ba5aa3568827ff0bb21
5oN1A <guarino.sonia@gmail.com>
2021-10-08 14:36:20 +0200

    files setting + preliminary analysis

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*
+1.n. km da percorrere : creare prompt
+2.età passeggero: creare prompt
+3.verifica  dati del prompt
+4.calcolo prezzo del biglietto in base ai km 
+5.applicazione degli eventuali sconti in riferimento all'età passeggero
+6.arrotondo i valori del prezzo finale
+
+*/
