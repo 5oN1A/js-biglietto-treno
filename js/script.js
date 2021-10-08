19bec5364bb8cfadf8bdd9fccb9289eb11c631fb
5oN1A <guarino.sonia@gmail.com>
2021-10-08 17:59:04 +0200

    insert year of birth

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-/*
-1.n. km da percorrere : creare prompt
-2.età passeggero: creare prompt
-3.verifica  dati del prompt
-4.calcolo prezzo del biglietto in base ai km 
-5.applicazione degli eventuali sconti in riferimento all'età passeggero
-6.arrotondo i valori del prezzo finale
-*/
 
 let tripLenght = parseInt(prompt("Inserisci il numero di Kilometri da percorrere"));
 
@@ -13,29 +5,45 @@ if (isNaN(tripLenght)) {
     alert("il valore inserito non è valido");
 }
 
-let userAge = parseInt(prompt("inserisci la tua età"));
+let userYOB = parseInt(prompt("inserisci il tuo anno di nascita"));
+
+if (isNaN(userYOB)) {
+    alert("il valore inserito non è valido");
+}
+
+let year = new Date();
+let currentYear = year.getFullYear();
+console.log(currentYear);
+
+let userAge = currentYear - userYOB;
+
+console.log(userAge);
 
 let tripPrice = tripLenght * 0.21;
 
 let discountedPrice;
 
-let msgDiscounted = "il prezzo del tuo biglietto scontato è di €"
+let message;
 
-let msgFullPrice = "il prezzo del tuo biglietto è di €"
+let msgFullPrice = "Il prezzo del tuo biglietto è di € ";
 
-if (isNaN(userAge)) {
-    alert("il valore inserito non è valido");
+let msgDiscounted20 = "Hai diritto ad uno sconto del 20%. Il prezzo del tuo biglietto è di € ";
+
+let msgDiscounted40 = "Hai diritto ad uno sconto del 40%. Il prezzo del tuo biglietto è di € ";
+
+
+
+if (userAge < 18) {
+    discountedPrice = tripPrice - (tripPrice * 0.2);
+    message = "Hai diritto ad uno sconto del 20%. Il prezzo del tuo biglietto è di € ";
+    alert(message + discountedPrice.toFixed(2));
+} else if (userAge >= 65) {
+    discountedPrice = tripPrice - (tripPrice * 0.4);
+    message = "Hai diritto ad uno sconto del 40%. Il prezzo del tuo biglietto è di € ";
+    alert(message + discountedPrice.toFixed(2));
 } else {
-    if (userAge < 18) {
-        discountedPrice = tripPrice - (tripPrice * 0.2);
-        alert(msgDiscounted + discountedPrice.toFixed(2));
-    } else if (userAge >= 65) {
-        discountedPrice = tripPrice - (tripPrice * 0.4);
-        alert(msgDiscounted + discountedPrice.toFixed(2));
-
-    } else {
-        alert(msgDiscounted + tripPrice.toFixed(2));
-    }
+    message = "Il prezzo del tuo biglietto è di € "
+    alert(message + tripPrice.toFixed(2));
 }
 
 
